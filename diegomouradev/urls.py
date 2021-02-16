fa4d935d232c1827ba21822a7d55f9530fdaf776
Diego Moura <59176835+diegomouradev@users.noreply.github.com>
2021-02-16 10:24:22 -0500

    corrected view to projects_index

--- diegomouradev/urls.py
+++ diegomouradev/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', include('projects.urls')),
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path("blog/", include("blog.urls")),
